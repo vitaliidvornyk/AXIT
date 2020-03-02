367e786e5fd831f58d92602d17c1eb74cf068dcd
vitaliidvornyk <59489402+vitaliidvornyk@users.noreply.github.com>
2020-03-02 03:11:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Tabs
+
+(function($) {
+    $(function() {     
+      $('ul.sentence__tabs').on('click', 'li:not(.sentence__tab_active)', function() {
+        $(this)
+          .addClass('sentence__tab_active').siblings().removeClass('sentence__tab_active')
+          .closest('div.container').find('div.sentence__content').removeClass('sentence__content_active').eq($(this).index()).addClass('sentence__content_active');
+      });     
+    });
+})(jQuery);
+
+// Hamburger
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.navbar__menu'),
+  menuItem = document.querySelectorAll('.navbar__menu_item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('navbar__menu_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('navbar__menu_active');
+      })
+  })
+})
+
+// Scroll up
+
+$(window).on( 'load', function() {
+  $('html, body').animate({scrollTop : 0});
+});
